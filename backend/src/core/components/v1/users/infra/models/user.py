18912489c0b9547d89c45ec26a1332e926bb7642
Gisela Mariano <gisela-mariano@hotmail.com>
2025-09-01 14:43:45 -0300

    feat(user): create users table

---
+++ backend/src/core/components/v1/users/infra/models/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy import (
+    JSON,
+    TIMESTAMP,
+    UUID,
+    Boolean,
+    CheckConstraint,
+    Column,
+    Index,
+    Integer,
+    String,
+    Table,
+    text,
+)
+from sqlalchemy.sql import func
+from src.core.persistence.database.sqlalchemy import metadata
+
+users = Table(
+    "users",
+    metadata,
+    Column(
+        name="id",
+        type_=UUID,
+        primary_key=True,
+        index=True,
+        server_default=text("gen_random_uuid()"),
+    ),
+    Column(name="name", type_=String(length=255), nullable=False),
+    Column(name="lastname", type_=String(length=255), nullable=False),
+    Column(name="username", type_=String(length=255), nullable=False),
+    Column(name="email", type_=String(length=60), nullable=False),
+    Column(name="password", type_=String(length=255), nullable=False),
+    Column(name="is_active", type_=Boolean, nullable=False, default=True),
+    Column(
+        name="created_at", type_=TIMESTAMP, server_default=func.now(), nullable=False
+    ),
+    Column(name="updated_at", type_=TIMESTAMP, onupdate=func.now()),
+    Column(name="deleted_at", type_=TIMESTAMP),
+)
---
+++ backend/src/core/persistence/database/models.py
@@ -0,0 +1,1 @@
+from src.core.components.v1.users.infra.models.user import users  # noqa
